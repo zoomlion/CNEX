0f61b9168f54e0c3886fdf491055f333041bcf92
JiangminZheng <zhengjiangmin@mail.nwpu.edu.cn>
2024-12-23 16:00:46 +0800

    better parallelization implementation

--- src/confi_mer.py
+++ src/confi_mer.py
@@ -9,10 +9,47 @@ import os
 import sys
 import re
 import argparse
-import math
+import string
+import random
+import shutil
 from collections import defaultdict
-import tqdm
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool
+
+def mer2int(mer, k):
+    result = 0
+    for i in range(k):
+        base = mer[i]
+        if base == 'A':
+            value = 0b00
+        elif base == 'C':
+            value = 0b01
+        elif base == 'G':
+            value = 0b10
+        elif base == 'T':
+            value = 0b11
+        else:
+            raise ValueError(f"Invalid base in k-mer: {base}")
+        result = (result << 2) | value
+    return result
+
+def int2mer(value, k):
+    mer = []
+    for _ in range(k):
+        base_value = value & 0b11
+        if base_value == 0b00:
+            base = 'A'
+        elif base_value == 0b01:
+            base = 'C'
+        elif base_value == 0b10:
+            base = 'G'
+        elif base_value == 0b11:
+            base = 'T'
+        else:
+            raise ValueError("Invalid value during conversion.")
+        mer.append(base)
+        value >>= 2
+    return ''.join(mer[::-1])
+
 
 def bunch2fas(bunch: str) -> dict:
     """
@@ -68,6 +105,8 @@ def calculate_entropy(sequence):
 
     # Calculate probabilities and entropy
     counts = {nucleotide: sequence.count(nucleotide) for nucleotide in 'ATCG'}
+    if set(counts.keys()) - set('ATCG'):
+        return 0
     entropy = -sum(
         (count / total) * log2(count / total) for count in counts.values() if count > 0
     )
@@ -100,28 +139,45 @@ def process_bunch(args):
     """
     Process a single bunch to identify informative mers.
     """
-    bunch_id, bunch, mer_size = args
-    local_affi = {}
-    local_count = defaultdict(int)
+    msa, mer_size, thread_id, total_threads, output = args
+    mer_affi = defaultdict(list)
+    mer_count = defaultdict(int)
+    for bunch_id, bunch in enumerate(bunch_generator(msa)):
+        local_affi = defaultdict(list)
+        for header, fas in bunch2fas(bunch).items():
+            if header.startswith('ref'):
+                continue
+            for loci, mer in generate_mers(fas, mer_size):
+                inted_mer = mer2int(mer, mer_size)
+                if inted_mer % total_threads == thread_id and \
+                    calculate_entropy(mer) >= 1.4:
+                    local_affi[mer].append((bunch_id, loci))
+        for mer, locations in local_affi.items():
+            inted_mer = mer2int(mer, mer_size)
+            set_locations = set(locations)
+            if len(locations) == mer_count.get(inted_mer, 1):
+                mer_count[inted_mer] = len(locations)
+                if len(set_locations) == 1:
+                    mer_affi[inted_mer].append(list(locations[0])+[len(locations)])
+            elif len(locations) > mer_count.get(inted_mer, 1):
+                mer_count[inted_mer] = len(locations)
+                mer_affi[inted_mer] = [list(locations[0])+[len(locations)]]
 
-    for header, fas in bunch2fas(bunch).items():
-        if header.startswith('ref'):
-            continue
-        for loci, mer in generate_mers(fas, mer_size):
-            if calculate_entropy(mer) >= 1.4:
-                local_affi.setdefault(mer, set())
-                local_affi[mer].add((bunch_id, loci))
-                local_count[mer] += 1
+    random.seed(42) # stable random
+    # save to output tsv
+    with open(output, 'w') as f:
+        for inted_mer, info in mer_affi.items():
+            # random select one with seed 42
+            selected_info = random.choice(info)
+            f.write("{0}\t{1}\t{2}\t{3}\n".format(
+                int2mer(inted_mer, mer_size),
+                selected_info[0],
+                selected_info[1],
+                selected_info[2]
+            ))
 
-    return {
-        mer: (
-            str(list(locations)[0][0]), 
-            str(list(locations)[0][1]), 
-            str(local_count[mer]))
-            for mer, locations in local_affi.items() if len(locations) == 1
-    }
 
-def map_mer(msa, mer_size, output, threads=4):
+def map_mer(msa, mer_size, temp, threads):
     """
     Read MSA alignment and pick up most informative mers into a dictionary.
 
@@ -132,40 +188,46 @@ def map_mer(msa, mer_size, output, threads=4):
     Returns:
         dict: Dictionary of mers and their associated information.
     """
-    mer_affi = {}
-    bunches = [(bunch_id, bunch, mer_size) for bunch_id, bunch in enumerate(bunch_generator(msa))]
+    pool = Pool(processes=threads)
+    for i in range(threads):
+        output_i = f"{temp}/mers_{i}.tsv"
+        pool.apply_async(
+            process_bunch,
+            args=((msa, mer_size, i, threads, output_i),)
+        )
+    pool.close()
+    pool.join()
 
-    # Use multiprocessing to process bunches in parallel
-    with Pool(processes=threads) as pool:
-        results = pool.map(process_bunch, bunches)
-
-    for local_affi in results:
-        for mer, locations in local_affi.items():
-            # find existing mer info
-            prev = mer_affi.get(mer, None)
-            if prev:
-                # update existing mer info
-                if int(prev[2]) < int(locations[2]):
-                    mer_affi[mer] = locations
-            else:
-                mer_affi[mer] = locations
-    with open(output, 'w') as f:
-        for mer, info in mer_affi.items():
-            # write to tsv
-            f.write("{0}\t{1}\n".format(mer, '\t'.join(info)))
-
-    return 0
 
 def main():
     parser = argparse.ArgumentParser(description="Build confident kmers from MSA alignment.")
     parser.add_argument("msa", type=str, help="Path to MSA file.")
     parser.add_argument("-k", "--mer_size", type=int, default=13, help="Size of the kmers (default: 13).")
-    parser.add_argument("-t", "--threads", type=int, default=12, help="threads number for confident kmer building")
+    parser.add_argument("-t", "--threads", type=int, default=8, help="threads number for confident kmer building")
     parser.add_argument("-o", "--output", type=str, default="mers_table.tsv", help="Path to the output TSV file.")
     args = parser.parse_args()
 
-    map_mer(args.msa, mer_size=args.mer_size, output=args.output, threads=args.threads)
+    # make random temp dir (8 random chars)
+    temp = os.path.join(
+        os.getcwd(), 
+        ''.join(random.choices(string.ascii_uppercase, k=8))
+    )
+    os.makedirs(temp, exist_ok=True)
+    map_mer(args.msa, mer_size=args.mer_size, temp=temp, threads=args.threads)
+    # cat all mers_*.tsv to output
+    with open(args.output, 'w') as f:
+        for i in range(args.threads):
+            output_i = f"{temp}/mers_{i}.tsv"
+            if not os.path.exists(output_i):
+                continue
+            with open(output_i, 'r') as fi:
+                shutil.copyfileobj(fi, f)
+            os.remove(output_i)
+    shutil.rmtree(temp)
 
 
 if __name__ == '__main__':
     main()
+
+
+
