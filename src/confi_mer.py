31d2589cd8252e6cd6266d9a56f8b71f30c8dd2d
JiangminZheng <zhengjiangmin@mail.nwpu.edu.cn>
2025-01-03 21:46:13 +0800

    optimized confi_mer method

--- src/confi_mer.py
+++ src/confi_mer.py
@@ -15,20 +15,15 @@ import shutil
 from collections import defaultdict
 from multiprocessing import Pool
 
+BASE_TO_INT = {'A': 0b00, 'C': 0b01, 'G': 0b10, 'T': 0b11}
+INT_TO_BASE = ['A', 'C', 'G', 'T']
+
 def mer2int(mer, k):
     result = 0
-    for i in range(k):
-        base = mer[i]
-        if base == 'A':
-            value = 0b00
-        elif base == 'C':
-            value = 0b01
-        elif base == 'G':
-            value = 0b10
-        elif base == 'T':
-            value = 0b11
-        else:
-            raise ValueError(f"Invalid base in k-mer: {base}")
+    for base in mer:
+        value = BASE_TO_INT.get(base)
+        if value is None:  # Handle invalid characters
+            return None
         result = (result << 2) | value
     return result
 
@@ -36,24 +31,16 @@ def int2mer(value, k):
     mer = []
     for _ in range(k):
         base_value = value & 0b11
-        if base_value == 0b00:
-            base = 'A'
-        elif base_value == 0b01:
-            base = 'C'
-        elif base_value == 0b10:
-            base = 'G'
-        elif base_value == 0b11:
-            base = 'T'
-        else:
-            raise ValueError("Invalid value during conversion.")
-        mer.append(base)
+        mer.append(INT_TO_BASE[base_value])
         value >>= 2
-    return ''.join(mer[::-1])
+    mer.reverse()  # Reverse in place for better performance
+    return ''.join(mer)
 
 
 def bunch2fas(bunch: str) -> dict:
     """
-    Convert bunch string to fas_dict, with key as seq_id and value as seq_str.
+    Convert a bunch string to a FASTA dictionary, with sequence IDs as keys and sequences as values.
+    Sequences with only '-' are ignored.
     """
     fas_dict = {}
     for line in bunch.split('\n'):
@@ -65,9 +52,11 @@ def bunch2fas(bunch: str) -> dict:
         else:
             fas_dict[seq_id] += line.strip().upper()
 
-    # Remove lines with no ATCG values
+    # Remove sequences with no valid characters (e.g., only '-')
     return {
-        id: seq for id, seq in fas_dict.items() if len(set(seq)) > 1
+        seq_id: seq
+        for seq_id, seq in fas_dict.items()
+        if len(set(seq) - {'-'}) > 0  # Exclude sequences with only '-'
     }
 
 def bunch_generator(fasta_file):
@@ -139,18 +128,26 @@ def process_bunch(args):
     """
     Process a single bunch to identify informative mers.
     """
-    msa, mer_size, thread_id, total_threads, output = args
+    msa, mer_size, min_c, thread_id, total_threads, output = args
     mer_affi = defaultdict(list)
     mer_count = defaultdict(int)
     for bunch_id, bunch in enumerate(bunch_generator(msa)):
+        # print "\b"* 20 to show progress
+        print(f"\r{bunch_id+1}", end='', flush=True)
         local_affi = defaultdict(list)
-        for header, fas in bunch2fas(bunch).items():
+        fas_dict = bunch2fas(bunch)
+        if len(fas_dict) < min_c+1:
+            continue
+        for header, fas in fas_dict.items():
             if header.startswith('ref'):
                 continue
             for loci, mer in generate_mers(fas, mer_size):
                 inted_mer = mer2int(mer, mer_size)
-                if inted_mer % total_threads == thread_id and \
-                    calculate_entropy(mer) >= 1.4:
+                if inted_mer is None:
+                    continue
+                if not inted_mer % total_threads == thread_id:
+                    continue
+                if calculate_entropy(mer) >= 1.4:
                     local_affi[mer].append((bunch_id, loci))
         for mer, locations in local_affi.items():
             inted_mer = mer2int(mer, mer_size)
@@ -177,7 +174,7 @@ def process_bunch(args):
             ))
 
 
-def map_mer(msa, mer_size, temp, threads):
+def map_mer(msa, mer_size, min_c, temp, threads):
     """
     Read MSA alignment and pick up most informative mers into a dictionary.
 
@@ -193,10 +190,12 @@ def map_mer(msa, mer_size, temp, threads):
         output_i = f"{temp}/mers_{i}.tsv"
         pool.apply_async(
             process_bunch,
-            args=((msa, mer_size, i, threads, output_i),)
+            args=((msa, mer_size, min_c, i, threads, output_i),)
         )
     pool.close()
     pool.join()
+
+    # process_bunch((msa, mer_size, 0, threads, f"{temp}/mers_0.tsv"))
 
 
 def main():
@@ -204,8 +203,17 @@ def main():
     parser.add_argument("msa", type=str, help="Path to MSA file.")
     parser.add_argument("-k", "--mer_size", type=int, default=13, help="Size of the kmers (default: 13).")
     parser.add_argument("-t", "--threads", type=int, default=8, help="threads number for confident kmer building")
+    parser.add_argument("-c", "--min_c", type=int, default=4, help="minimum count of the kmers (default: 10).")
     parser.add_argument("-o", "--output", type=str, default="mers_table.tsv", help="Path to the output TSV file.")
     args = parser.parse_args()
+
+    # total number is the '###' line count input
+    total_num = os.popen(f"grep -c '###' {args.msa}").read().strip()
+    if not total_num.isdigit():
+        print(f"Invalid input file: {args.msa}")
+        sys.exit(1)
+    total_num = int(total_num)
+    print(f"Total number of bunches: {total_num}")
 
     # make random temp dir (8 random chars)
     temp = os.path.join(
@@ -213,7 +221,7 @@ def main():
         ''.join(random.choices(string.ascii_uppercase, k=8))
     )
     os.makedirs(temp, exist_ok=True)
-    map_mer(args.msa, mer_size=args.mer_size, temp=temp, threads=args.threads)
+    map_mer(args.msa, mer_size=args.mer_size, min_c=args.min_c, temp=temp, threads=args.threads)
     # cat all mers_*.tsv to output
     with open(args.output, 'w') as f:
         for i in range(args.threads):
@@ -228,6 +236,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
